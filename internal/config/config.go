9f43477436753319e0144dd0fdad3de09979a805
henok-tesfu <henoktesf48@gmail.com>
2024-11-01 09:49:27 +0300

    database set up

---
+++ internal/config/config.go
@@ -0,0 +1,30 @@
+package config
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var DB *sql.DB
+
+func ConnectDatabase() {
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true",
+		os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_NAME"))
+
+	var err error
+	DB, err = sql.Open("mysql", dsn)
+	if err != nil {
+		log.Fatalf("Error opening database: %v", err)
+	}
+
+	if err = DB.Ping(); err != nil {
+		log.Fatalf("Error connecting to database: %v", err)
+	}
+
+	log.Println("Database connection established")
+}
